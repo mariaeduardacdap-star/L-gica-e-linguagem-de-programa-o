8c62bcd7bef56da04d1311bb592aafa27569c8fd
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-06-11 13:03:51 -0300

    Add files via upload

---
+++ area.py
@@ -0,0 +1,6 @@
+largura= float(input("Digite a largura: ")) #variável que permite números quebrados
+comprimento= float(input("Digite o comprimento: ")) #variável que permite números
+
+area= largura * comprimento #fórmula para calcular a área
+
+    print("Área: ", area) #aparece o resultado
---
+++ velocidade.py
@@ -0,0 +1,6 @@
+distancia= float(input("Digite a distância: ")) #variável de número quebrado
+tempo= float(input("Digite o tempo: ")) #variável de número quebrado
+
+velocidade=distancia/tempo
+
+print("Velocidade: ", velocidade)
