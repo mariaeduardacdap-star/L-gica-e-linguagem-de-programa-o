ab25c21291baf2de2a1377863015eec21e51dcab
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-06-16 12:57:31 -0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,4 +1,8 @@
-numero = int(input("Digite um número: "))
+nota1 = int(input("Digite a nota do 1° Bimestre: ")) # variável de 
+nota2 = int(input("Digite a nota do 2° Bimestre: ")) # variável de
+nota3 = int(input("Digite a nota do 3° Bimestre: ")) # variável de 
+nota4 = int(input("Digite a nota do 4° Bimestre: ")) # variável de
 
-resultado = numero*2
-print("Resultado: ", resultado)
+media = (nota1 + nota2 + nota3 + nota4)/4 # fórmula que soma toda
+
+print("sua nota é: ", media) # mostra a média do ano
