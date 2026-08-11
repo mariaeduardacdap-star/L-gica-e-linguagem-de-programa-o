0d1795d6cda143461fc5e89772c79dcbede8d364
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-08-11 13:09:18 -0300

    Add files via upload

---
+++ energiapotgrav.py
@@ -0,0 +1,8 @@
+input("Complete os dados a seguir e descubra a Energia Potencial Gravitacional.")
+
+massa = float(input("Digite a massa: "))
+gravidade = float(input("Digite o valor da gravidade: "))
+altura = float(input("Digite a altura: "))
+
+energia = massa*gravidade*altura
+print("A energia potencial gravitacional é igual a: ", energia)
---
+++ regra3.py
@@ -0,0 +1,7 @@
+input("Siga os valores a seguir para obter o resultado d regra de 3.")
+valor1 = float(input("Digite o primero valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+valor3 = float(input("Digite o terceiro valor: "))
+
+resultado = (valor2*valor3)/valor1
+print("O resultado é: ", resultado)
