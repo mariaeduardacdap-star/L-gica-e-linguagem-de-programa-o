3f1424d5e199cac41b5feea16aec2c832afd6e1c
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-09-10 12:46:46 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,7 @@
+numero = float(input("Digite para descubrir o dobro de determinado valor: "))
+atividade1 = numero * 2
+    print("O dobro é", atividade1)
+nota1 = float(input("Digite as notas a seguir para saber a média. Nota 1: "))
+nota2 = float(input("Nota 2: "))
+nota3 = float(input("Nota 3: "))
+media = ((nota1 + nota2 + nota3)/3)
