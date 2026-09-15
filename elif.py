25364fb3b17d2e354b41eadc2b45e1ddb63fb1f1
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-09-15 12:45:50 -0300

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade: "))
+if idade >=60:
+    print("Idoso")
+elif idade >=18:
+    print("Adulto")
+elif idade >=13:
+    print("Adolescente")
+else:
+    print("Criança")
