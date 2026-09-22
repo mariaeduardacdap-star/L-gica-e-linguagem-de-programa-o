bc62bf7495460a016e847ee2b4e1a8bf3fa6e786
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-09-22 13:03:40 -0300

    Add files via upload

---
+++ temperatura.py
@@ -0,0 +1,10 @@
+temperatura = int(input("Digite a temperatura: "))
+
+if temperatura >= 35:
+    print("Muito quente!")
+elif temperatura >= 30:
+    print("Quente!")
+elif temperatura >=18:
+    print("Fresco.")
+else:
+    print("Frio.")
---
+++ zero.py
@@ -0,0 +1,7 @@
+numero = int(input("Descubra se o número é negativo ou positvo: "))
+if numero >=1:
+    print("Positivo")
+elif numero == 0:
+    print("Zero")
+else:
+    print("Negativo")
