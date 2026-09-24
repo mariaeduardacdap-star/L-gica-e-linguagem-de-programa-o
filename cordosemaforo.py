ae72f91d025aec9cff3928ff9ea10f74c594bb9e
mariaeduardacdap-star <mariaeduardacdap@gmail.com>
2026-09-24 12:56:16 -0300

    Add files via upload

---
+++ cordosemaforo.py
@@ -0,0 +1,7 @@
+cor_do_semaforo = input("Qual a cor do semáforo? ")
+if cor_do_semaforo =="Verde":
+    print("Siga em frente")
+elif cor_do_semaforo =="Amarelo":
+    print("Atenção")
+else: 
+    print("Espere o sinal ficar verde")
---
+++ login.py
@@ -0,0 +1,5 @@
+login = input("Digite seu e-mail: ")
+if login =="mariaeduardacdap@gmail.com":
+    print("E-mail correto")
+else:
+    print("O e-mail é invalido.")
